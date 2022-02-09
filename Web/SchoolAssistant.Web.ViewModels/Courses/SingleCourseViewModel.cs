9149d9ea41c00ae42820fa7959962fb84a728c4c
Georgi Tsekov <georgi_tsekov@abv.bg>
2022-02-09 17:47:41 +0200

    Implement course Voting with AJAX and jquery

--- Web/SchoolAssistant.Web.ViewModels/Courses/SingleCourseViewModel.cs
+++ Web/SchoolAssistant.Web.ViewModels/Courses/SingleCourseViewModel.cs
@@ -2,11 +2,13 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
+    using AutoMapper;
     using SchoolAssistant.Data.Models;
     using SchoolAssistant.Services.Mapping;
 
-    public class SingleCourseViewModel : IMapFrom<Course>
+    public class SingleCourseViewModel : IMapFrom<Course>, IHaveCustomMappings
     {
         public int Id { get; set; }
 
@@ -18,8 +20,17 @@
 
         public string CreatedByUserUserName { get; set; }
 
+        public double AverageVote { get; set; }
+
         public DateTime CreatedOn { get; set; }
 
         public IEnumerable<SingleLectureViewModel> Lectures { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<Course, SingleCourseViewModel>()
+                .ForMember(x => x.AverageVote, opt =>
+                    opt.MapFrom(x => x.Votes.Count() == 0 ? 0 : x.Votes.Average(v => v.Value)));
+        }
     }
 }
